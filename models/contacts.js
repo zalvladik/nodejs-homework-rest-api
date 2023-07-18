e5c83b04f0f7ce0bcc73e3280bb45d6047c42169
zalvladik <zalipkav@ukr.net>
2023-07-18 13:41:38 +0300

    okey

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,82 @@
-// const fs = require('fs/promises')
+import fs from 'fs/promises'
+import path from 'path'
+import {nanoid} from "nanoid";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("models", "contacts.json")
 
-const getContactById = async (contactId) => {}
+const updateContactsStorage = contact => fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
 
-const removeContact = async (contactId) => {}
+const listContacts = async () => {
+    // ...твій код. Повертає масив контактів.
+    const data = await fs.readFile(contactsPath)
+    return JSON.parse(data)
+  }
+  
+const getContactById = async (contactId) => {
+    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+    const contacts = await listContacts()
+    const result = contacts.find(contact => contact.id === contactId )
+  
+    return result
+  }
+  
+const removeContact = async (contactId) => {
+    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
 
-const addContact = async (body) => {}
+    const contacts = await listContacts()
+    const result = contacts.find( contact => contact.id === contactId ) 
+    if(!result){
+      return result
+    }
+    const newArray = contacts.filter( contact => contact.id !== contactId ) 
 
-const updateContact = async (contactId, body) => {}
+    await updateContactsStorage(newArray)
+    return result
+  }
 
-module.exports = {
+    const addContact = async ({name, email, phone}) => {
+    // ...твій код. Повертає об'єкт доданого контакту.
+    const newArray = await listContacts()
+    const newContact = {
+        id:nanoid(),
+        name,
+        email,
+        phone,
+    };
+
+    newArray.push(newContact)
+
+    await updateContactsStorage(newArray)
+    return newContact
+  }
+
+    const updateContact = async (contactId, data) => {
+      const contacts = await listContacts()
+
+      const result = contacts.find( contact => contact.id === contactId ) 
+          if(!result){
+            return result
+          }
+
+      let putContact = null
+      const newArray = await contacts.map(contact => 
+        { if(contact.id === contactId){
+          putContact = {...contact,...data}
+          return contact = {...contact,...data}
+        }
+        return contact
+      }
+      )
+      await updateContactsStorage(newArray)
+      return putContact
+    }
+
+const todoActions = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
 }
+
+export default todoActions
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,93 @@
-const express = require('express')
+import express from 'express'
+import todoActions from '../../models/contacts.js'
+import Joi from 'joi'
 
 const router = express.Router()
+const addSchema = Joi.object({
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        phone: Joi.string().required(),
+})
+
+const HttpError = (status, message) => {
+  const error = new Error(message)
+  error.status = status
+  return error
+}
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message ' })
+  try {
+    const result = await todoActions.listContacts()
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params
+    const result = await todoActions.getContactById(contactId)
+
+    if(!result){
+      throw HttpError(404, "Not Found")
+    }
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addSchema.validate(req.body)
+
+    if(error){
+      throw HttpError(400, 'missing required name field')
+    }
+
+    const result = await todoActions.addContact(req.body)
+
+    res.status(201).json(result)
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {contactId} = req.params
+    const result = await todoActions.removeContact(contactId)
+
+    if(!result){
+      throw HttpError(404,'Not found')
+    }
+    
+    res.status(200).json({"message": "contact deleted"})
+  } catch (error) {
+    next(error)
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = addSchema.validate(req.body)
+
+    if(error){
+      throw HttpError(400,'missing fields')
+    }
+
+    const {contactId} = req.params
+    const result = await todoActions.updateContact(contactId, req.body)
+
+    if(!result){
+      throw HttpError(404,'Not found') 
+    }
+
+    res.status(200).json({"message": "contact update"})
+  } catch (error) {
+    next(error) 
+  }
 })
 
-module.exports = router
+export default router
