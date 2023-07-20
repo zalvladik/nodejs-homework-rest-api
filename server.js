2bf252e43d68e821fbef5aeff13ac243371f25ac
zalvladik <zalipkav@ukr.net>
2023-07-20 14:36:35 +0300

    kk

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import app from './app.js'
 import mongoose from 'mongoose'
-import DB_HOST from './config.js'
 
+mongoose.set('strictQuery', true)
+const DB_HOST = "mongodb+srv://ek11vladislav:rm4hoHhXoCnAfpGE@cluster0.kyiu4w8.mongodb.net/db-contacts?retryWrites=true&w=majority"
 mongoose.connect(DB_HOST)
   .then(() => 
   app.listen(3000, () => {
