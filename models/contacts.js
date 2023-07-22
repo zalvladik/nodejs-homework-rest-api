7024b6ccf9c0a8637613486a410fe147db075bbc
zalvladik <zalipkav@ukr.net>
2023-07-22 17:30:02 +0300

    kk

--- models/contacts.js
+++ models/contacts.js
@@ -17,3 +17,41 @@ module.exports = {
   addContact,
   updateContact,
 }
+import { Schema, model } from 'mongoose';
+import handleMongooseError from '../helpers/handleMongooseError'
+import Joi from 'joi'
+
+const contactSchema = new Schema({
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter"
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    token: String
+  },{versionKey: false})
+
+contactSchema.post( "save", handleMongooseError)
+
+const phoneNumberValid =/^\(\d{3}\) \d{3}-\d{4}$/
+
+export const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().pattern(phoneNumberValid).required(),
+  owner: Joi.string().required(),
+})
+
+export const Contact = model('contact', contactSchema)
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const app = require('./app')
+import app from './app.js'
+import mongoose from 'mongoose'
+import app from "./app"
+
+const {DB_HOST} = null
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
